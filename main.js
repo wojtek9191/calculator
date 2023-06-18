ec93205f4a564251b909b50a6af380ebb4a074d9
Wojciech Miczolek <wojciechm2291@gmail.com>
2023-06-18 10:25:13 +0100

    Change layout of buttons and start over with JS

---
+++ main.js
@@ -0,0 +1,15 @@
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+
+
+const numberButtons = document.querySelectorAll(".btn");
+const zeroButton = document.querySelector("#btn0");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#btnC");
+const equalsButton = document.querySelector("#btnEquals");
+const display = document.querySelector(".display");
+const previousDisplay = document.querySelector("#previous");
+const currentDisplay = document.querySelector("#current");
+
+
