58143ca7974248c66ec3c84878077ad3c21721b1
Wojciech Miczolek <wojciechm2291@gmail.com>
2023-07-10 21:07:25 +0100

    Add basic functions to all buttons

--- main.js
+++ main.js
@@ -13,10 +13,69 @@ equalsButton.addEventListener('click', showResult);
 clearButton.addEventListener('click', clearDisplay);
 
 
-
-
-
-
-
-
 let result = '';
+
+
+function displayNumbers() {
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0';
+
+    currentNumber.innerHTML += this.textContent;
+}
+
+
+function operate() {
+    if(currentNumber.innerHTML === '' && this.textContent === '-'){
+        currentNumber.innerHTML = '-';
+        return;
+    }
+
+    if(this.textContent === '-' && currentNumber.innerHTML.includes('-')) return;
+
+    else if(currentNumber.innerHTML === '') return;
+
+    if(displayOperator.innerHTML !== '') {
+        showResult();
+    }
+
+    previousNumber.innerHTML = currentNumber.innerHTML;
+    displayOperator.innerHTML = this.textContent;
+    currentNumber.innerHTML = '';
+}
+
+
+function showResult() {
+    if(currentNumber.innerHTML === '' || previousNumber.innerHTML === '') return;
+
+    let current = Number(currentNumber.innerHTML);
+    let previous = Number(previousNumber.innerHTML);
+    let operator = displayOperator.innerHTML;
+
+
+    switch(operator) {
+        case '+':
+            result = current + previous;
+            break;
+        case '-':
+            result = previous - current;
+            break;
+        case 'x':
+            result = current * previous;
+            break;
+        case '/':
+            result = previous / current;
+            break;
+    }
+
+    currentNumber.innerHTML = result;
+    previousNumber.innerHTML = '';
+    displayOperator.innerHTML = '';
+}
+
+
+function clearDisplay() {
+    previousNumber.innerHTML = '';
+    displayOperator.innerHTML = '';
+    currentNumber.innerHTML = '';
+    result = '';
+}
